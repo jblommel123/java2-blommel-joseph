66383e9ebc8e37fbe7318d3ad73b0cba7742813f
Joseph Blommel <jblommel123@users.noreply.github.com>
2022-03-16 20:23:15 -0400

    Create UtilitiesTest.java
    added unit tests, need to implement

---
+++ test/edu/institution/asn5/UtilitiesTest.java
@@ -0,0 +1,27 @@
+package edu.institution.asn5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class UtilitiesTest {
+
+	@Test
+	void testLinearSearch() {
+		assertEquals(1, 1);
+		
+	}
+
+	@Test
+	void testListItems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveDuplicates() {
+		fail("Not yet implemented");
+	}
+
+}
